2222aaf28cef37936198d9a838954257ce5fd8e2
Jacobo <jacoborodicio@gmail.com>
2019-02-02 14:16:26 +0100

    Crazy time by setState

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data
+      name: 'demo',
+      data: data,
+      allFriends: []
     };
   }  
 
@@ -14,16 +16,33 @@ class App extends Component {
     const value = this.state.data[indexItem]["friends"].map((item, index) => {
       return <p key={index}>{item.name}</p>;
     });
-    console.log(value);
+    // console.log(value);
     return value; 
   }
 
+  componentDidMount() {
+    console.log(this.state.name);
+    this.extractAllFriends();  
+    console.log(this.state.name);
+  }
+  extractAllFriends = () => {
+    //  const friends =  this.state.data.map(item => {
+    //    console.log(item.friends);
+    //   return item.friends;
+
+    // });
+    // console.log("Friends: ",friends)
+    this.setState({name: 'jacobo'})}
+
   render() {
     const friends = this.extractFriendsItemByIndex(3);
-
+    // const allFriends = this.state.allFriends.friends.map(friend => <p>{friend}</p>)
+    // const allFriends = this.state.allFriends[0]
     return (
       <div className="App">
        {friends}
+       <hr/>
+       {/* {allFriends} */}
       </div>
     );
   }
