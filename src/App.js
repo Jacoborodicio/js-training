6412e215faaed5c057f41ff24ae6cf2aa92e0b60
Jacko <jacoborodicio@gmail.com>
2019-02-02 12:55:11 +0100

    Injecting html through .map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data/arrayObjectsDummy';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    };
+  }  
+
+  extractFriendsItemByIndex = indexItem => {
+    const value = this.state.data[indexItem]["friends"].map((item, index) => {
+      return <p key={index}>{item.name}</p>;
+    });
+    console.log(value);
+    return value; 
+  }
+
   render() {
+    const friends = this.extractFriendsItemByIndex(3);
+
     return (
       <div className="App">
-        
+       {friends}
       </div>
     );
   }
