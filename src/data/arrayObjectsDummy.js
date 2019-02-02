4a5e382beac222b38e59f02cc22af037e3a05948
Jacko <jacoborodicio@gmail.com>
2019-02-02 10:57:15 +0100

    Dummy data added

---
+++ src/data/arrayObjectsDummy.js
@@ -0,0 +1,462 @@
+const arrayObjectsDummy = 
+    [
+        {
+          "_id": "5c5567b7fd5419405cad4f56",
+          "index": 0,
+          "guid": "6ec85d9b-aff1-4ff4-8df7-b672f60e0037",
+          "isActive": true,
+          "balance": "$1,405.16",
+          "picture": "http://placehold.it/32x32",
+          "age": 31,
+          "eyeColor": "brown",
+          "name": {
+            "first": "Gina",
+            "last": "Melton"
+          },
+          "company": "DECRATEX",
+          "email": "gina.melton@decratex.name",
+          "phone": "+1 (941) 557-3697",
+          "address": "132 Schroeders Avenue, Ernstville, South Dakota, 539",
+          "about": "Ea laborum sunt sit aliqua exercitation aute eu proident Lorem adipisicing. Cupidatat duis mollit incididunt quis. Pariatur sint sit laborum Lorem dolor commodo sunt sit velit. Non amet laboris commodo elit do amet.",
+          "registered": "Wednesday, December 7, 2016 1:18 AM",
+          "latitude": "44.787821",
+          "longitude": "45.094394",
+          "tags": [
+            "sit",
+            "quis",
+            "eiusmod",
+            "dolor",
+            "anim"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Brennan Ferguson"
+            },
+            {
+              "id": 1,
+              "name": "Francis Alston"
+            },
+            {
+              "id": 2,
+              "name": "Goff Miranda"
+            }
+          ],
+          "greeting": "Hello, Gina! You have 7 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "5c5567b7a05f39173fb7c7ff",
+          "index": 1,
+          "guid": "b11717e8-3581-4d8f-824e-c0ba8ae3e1bb",
+          "isActive": false,
+          "balance": "$3,362.58",
+          "picture": "http://placehold.it/32x32",
+          "age": 29,
+          "eyeColor": "green",
+          "name": {
+            "first": "Katelyn",
+            "last": "Wong"
+          },
+          "company": "DOGNOST",
+          "email": "katelyn.wong@dognost.biz",
+          "phone": "+1 (931) 406-3196",
+          "address": "101 Dorchester Road, Ona, Alabama, 9954",
+          "about": "Tempor dolor do ex occaecat dolore culpa mollit excepteur id nisi. Aute ut cillum est velit dolore eiusmod consectetur aliquip. Sit ullamco elit officia quis esse magna ad sit culpa ipsum ut voluptate cupidatat exercitation. Anim exercitation excepteur non cillum deserunt occaecat. Pariatur exercitation veniam aliqua occaecat cillum est sunt laboris. Ullamco eiusmod officia amet ullamco eu et adipisicing ex nulla in dolore deserunt.",
+          "registered": "Wednesday, January 21, 2015 9:17 PM",
+          "latitude": "-83.99859",
+          "longitude": "-179.89448",
+          "tags": [
+            "cillum",
+            "Lorem",
+            "minim",
+            "consequat",
+            "velit"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Burgess Collins"
+            },
+            {
+              "id": 1,
+              "name": "Natalie Townsend"
+            },
+            {
+              "id": 2,
+              "name": "Helga Ferrell"
+            }
+          ],
+          "greeting": "Hello, Katelyn! You have 5 unread messages.",
+          "favoriteFruit": "apple"
+        },
+        {
+          "_id": "5c5567b7fe8fc50f1087d6f4",
+          "index": 2,
+          "guid": "8cde0e63-57a3-4c40-985b-f2204e5d1abc",
+          "isActive": false,
+          "balance": "$1,668.15",
+          "picture": "http://placehold.it/32x32",
+          "age": 38,
+          "eyeColor": "blue",
+          "name": {
+            "first": "Larsen",
+            "last": "Hammond"
+          },
+          "company": "UNCORP",
+          "email": "larsen.hammond@uncorp.org",
+          "phone": "+1 (870) 512-3027",
+          "address": "826 Ridge Court, Craig, Missouri, 4644",
+          "about": "Excepteur enim minim pariatur excepteur elit incididunt duis exercitation. Non deserunt eiusmod amet duis consequat occaecat enim voluptate excepteur voluptate minim proident. Dolor mollit nostrud aute ad occaecat id sit in consequat ullamco dolore veniam minim in. Fugiat sit occaecat nulla labore culpa. Consectetur ex do ut reprehenderit in est deserunt ad est incididunt.",
+          "registered": "Friday, January 5, 2018 10:19 AM",
+          "latitude": "-25.203025",
+          "longitude": "-163.66477",
+          "tags": [
+            "Lorem",
+            "ea",
+            "officia",
+            "enim",
+            "exercitation"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Brianna Maddox"
+            },
+            {
+              "id": 1,
+              "name": "Parker Stanley"
+            },
+            {
+              "id": 2,
+              "name": "Rodriquez Pena"
+            }
+          ],
+          "greeting": "Hello, Larsen! You have 6 unread messages.",
+          "favoriteFruit": "apple"
+        },
+        {
+          "_id": "5c5567b7046647fe54537c6a",
+          "index": 3,
+          "guid": "fdde16fc-b187-45ea-b572-5c8b418b8131",
+          "isActive": true,
+          "balance": "$1,829.28",
+          "picture": "http://placehold.it/32x32",
+          "age": 23,
+          "eyeColor": "green",
+          "name": {
+            "first": "Jefferson",
+            "last": "Lowe"
+          },
+          "company": "FURNITECH",
+          "email": "jefferson.lowe@furnitech.co.uk",
+          "phone": "+1 (800) 471-2460",
+          "address": "641 Alabama Avenue, Hall, Montana, 3804",
+          "about": "Culpa qui et et culpa amet. Esse sunt duis non ullamco ex. Cillum ex cupidatat magna non voluptate fugiat. Aliqua elit non amet pariatur amet ea culpa enim. Duis commodo reprehenderit occaecat nisi magna sit veniam deserunt pariatur ipsum officia. Do ullamco eu dolore in cillum. Ullamco Lorem incididunt dolor et dolor.",
+          "registered": "Sunday, November 9, 2014 9:00 PM",
+          "latitude": "-21.613552",
+          "longitude": "-123.107469",
+          "tags": [
+            "tempor",
+            "in",
+            "occaecat",
+            "Lorem",
+            "nostrud"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Miriam England"
+            },
+            {
+              "id": 1,
+              "name": "Willa Hansen"
+            },
+            {
+              "id": 2,
+              "name": "Mann Buckner"
+            }
+          ],
+          "greeting": "Hello, Jefferson! You have 6 unread messages.",
+          "favoriteFruit": "banana"
+        },
+        {
+          "_id": "5c5567b774f90921dc199115",
+          "index": 4,
+          "guid": "cb121c24-4113-4a18-8dc5-d2b33dc4f735",
+          "isActive": true,
+          "balance": "$1,575.40",
+          "picture": "http://placehold.it/32x32",
+          "age": 32,
+          "eyeColor": "blue",
+          "name": {
+            "first": "Guerra",
+            "last": "Cash"
+          },
+          "company": "ROBOID",
+          "email": "guerra.cash@roboid.info",
+          "phone": "+1 (831) 533-3373",
+          "address": "116 Just Court, Emison, Federated States Of Micronesia, 1001",
+          "about": "Do qui irure occaecat in proident sint exercitation exercitation est veniam excepteur magna. Qui dolore laboris aute sint in dolor proident et officia sunt do. Mollit consectetur voluptate reprehenderit mollit tempor amet commodo fugiat dolore aute do. Ad reprehenderit officia mollit ipsum mollit quis nulla laboris minim duis sint anim dolore. Adipisicing ullamco voluptate laboris minim occaecat Lorem non excepteur ad aliqua dolor do. Do labore occaecat ex dolor reprehenderit in ut esse. Dolor consectetur labore adipisicing amet excepteur id non proident labore do in cillum minim commodo.",
+          "registered": "Friday, October 12, 2018 12:10 AM",
+          "latitude": "-23.611226",
+          "longitude": "-139.354576",
+          "tags": [
+            "anim",
+            "enim",
+            "sunt",
+            "cillum",
+            "incididunt"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Garner Hurley"
+            },
+            {
+              "id": 1,
+              "name": "Sweeney Powell"
+            },
+            {
+              "id": 2,
+              "name": "Hartman Moore"
+            }
+          ],
+          "greeting": "Hello, Guerra! You have 6 unread messages.",
+          "favoriteFruit": "apple"
+        },
+        {
+          "_id": "5c5567b789e0aa76fe1a9922",
+          "index": 5,
+          "guid": "5d870c44-7246-47b6-ba9a-a81983414032",
+          "isActive": false,
+          "balance": "$3,865.74",
+          "picture": "http://placehold.it/32x32",
+          "age": 40,
+          "eyeColor": "blue",
+          "name": {
+            "first": "Nettie",
+            "last": "Cervantes"
+          },
+          "company": "KONGENE",
+          "email": "nettie.cervantes@kongene.com",
+          "phone": "+1 (803) 431-2513",
+          "address": "753 Nelson Street, Elliott, Arizona, 5803",
+          "about": "Laboris anim ullamco nisi excepteur. Duis reprehenderit ipsum laborum magna dolor exercitation cupidatat elit laborum laboris deserunt enim. Ipsum nulla laboris consectetur irure ipsum sit occaecat occaecat do.",
+          "registered": "Monday, April 14, 2014 7:57 AM",
+          "latitude": "-57.045453",
+          "longitude": "39.433554",
+          "tags": [
+            "eiusmod",
+            "velit",
+            "est",
+            "cillum",
+            "commodo"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Valarie Rios"
+            },
+            {
+              "id": 1,
+              "name": "Mcintyre Avila"
+            },
+            {
+              "id": 2,
+              "name": "Liliana Michael"
+            }
+          ],
+          "greeting": "Hello, Nettie! You have 6 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "5c5567b7e5b77b326c1f4d99",
+          "index": 6,
+          "guid": "7d2b9731-5be7-4cad-b532-60ed1e51ecd5",
+          "isActive": false,
+          "balance": "$3,145.14",
+          "picture": "http://placehold.it/32x32",
+          "age": 34,
+          "eyeColor": "brown",
+          "name": {
+            "first": "Deanne",
+            "last": "Chan"
+          },
+          "company": "PLAYCE",
+          "email": "deanne.chan@playce.tv",
+          "phone": "+1 (819) 588-2087",
+          "address": "907 Boerum Place, Drummond, Vermont, 9818",
+          "about": "Officia nulla voluptate culpa ad deserunt Lorem sit dolor enim dolor ipsum irure. Culpa id esse irure culpa dolor velit commodo voluptate ipsum nisi nisi. Dolor in ipsum amet aute elit non. Non do cillum excepteur sit aliquip commodo dolor magna. Enim aute amet ullamco magna nisi reprehenderit irure excepteur. Consectetur aliquip ad ea nisi sint laboris ad sunt officia quis enim. Incididunt laborum ad enim cupidatat velit consequat excepteur sint irure anim duis minim.",
+          "registered": "Tuesday, August 30, 2016 2:13 PM",
+          "latitude": "58.540564",
+          "longitude": "-18.519393",
+          "tags": [
+            "commodo",
+            "minim",
+            "esse",
+            "ullamco",
+            "mollit"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Walker Greer"
+            },
+            {
+              "id": 1,
+              "name": "Chelsea Terrell"
+            },
+            {
+              "id": 2,
+              "name": "Salas Gay"
+            }
+          ],
+          "greeting": "Hello, Deanne! You have 9 unread messages.",
+          "favoriteFruit": "strawberry"
+        },
+        {
+          "_id": "5c5567b75af331aa950bcb0b",
+          "index": 7,
+          "guid": "9649ea90-3f43-4143-b10e-0d4abdba84a9",
+          "isActive": false,
+          "balance": "$2,515.55",
+          "picture": "http://placehold.it/32x32",
+          "age": 37,
+          "eyeColor": "brown",
+          "name": {
+            "first": "Dona",
+            "last": "Ruiz"
+          },
+          "company": "ZYTREK",
+          "email": "dona.ruiz@zytrek.me",
+          "phone": "+1 (857) 435-2913",
+          "address": "648 Greenpoint Avenue, Suitland, Washington, 6200",
+          "about": "Cillum pariatur esse adipisicing est proident dolore ea Lorem eu occaecat aliqua. Id veniam ullamco laboris aliquip do excepteur dolore ad sit ex aliqua. Nisi ullamco reprehenderit velit ut excepteur eiusmod. Ullamco minim ea Lorem laborum do incididunt duis aliqua consectetur exercitation commodo. Excepteur tempor cillum pariatur adipisicing adipisicing minim do consequat culpa culpa ullamco nisi nulla sunt.",
+          "registered": "Saturday, March 31, 2018 9:11 AM",
+          "latitude": "-70.80313",
+          "longitude": "58.28135",
+          "tags": [
+            "excepteur",
+            "reprehenderit",
+            "consectetur",
+            "non",
+            "culpa"
+          ],
+          "range": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+          ],
+          "friends": [
+            {
+              "id": 0,
+              "name": "Eloise Velazquez"
+            },
+            {
+              "id": 1,
+              "name": "Meagan Schultz"
+            },
+            {
+              "id": 2,
+              "name": "Elaine Bowen"
+            }
+          ],
+          "greeting": "Hello, Dona! You have 6 unread messages.",
+          "favoriteFruit": "strawberry"
+        }
+      ]
+
+
+export default arrayObjectsDummy;
